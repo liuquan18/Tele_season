5a1982e9ffc51305aabc8976c6ec6898222c7123
Quan <quan.liu@mpimet.mpg.de>
2022-12-16 15:03:43 +0100

    all and first

--- script/1quick_plots/quick_plot.py
+++ script/1quick_plots/quick_plot.py
@@ -1,3 +1,26 @@
 #%%
 import src.quick_plot.sptial_profile_generator as quick_plot
 # %%
+# independent all pattern
+ind_all = quick_plot.first10_last10_index("ind","all")
+ind_all.plot_all()
+ind_all.create_doc()
+
+#%%
+# dependent all pattern
+ind_all = quick_plot.first10_last10_index("dep","all")
+ind_all.plot_all()
+ind_all.create_doc()
+
+# %%
+# independent first pattern
+ind_all = quick_plot.first10_last10_index("ind","first")
+ind_all.plot_all()
+ind_all.create_doc()
+
+# %%
+# dependent first pattern
+ind_all = quick_plot.first10_last10_index("dep","first")
+ind_all.plot_all()
+ind_all.create_doc()
+# %%
