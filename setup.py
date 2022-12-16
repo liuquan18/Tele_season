808412d560b43e696370415f9b7cb625c6b7bc4f
Quan <quan.liu@mpimet.mpg.de>
2022-12-16 12:24:34 +0100

    name of the package

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
-    version='0.0.2',
+    name='Tel_season',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'pytest',
